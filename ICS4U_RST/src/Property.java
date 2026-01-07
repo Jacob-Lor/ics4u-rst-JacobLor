e5ec98f8e7deb32632fbe674fbbe3dfcbe5ff27b
Jacob Lor <jacob.lor.main@gmail.com>
2026-01-07 14:05:57 -0500

    Created datafields for Monopoly game.

--- ICS4U_RST/src/Property.java
+++ ICS4U_RST/src/Property.java
@@ -1,5 +1,13 @@
 
 public class Property {
+	
+	private String name;
+	private int fee;
+	private boolean purchased;
+	private String owner;
+	private int inherentValue;
+	private boolean mortgaged;
+	private boolean monopolized;
 
 	public Property() {
 		// TODO Auto-generated constructor stub
