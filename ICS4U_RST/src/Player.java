f64e847c6d1c6a70d6d2d940013d66f4697dc7d8
Jacob Lor <jacob.lor.main@gmail.com>
2026-01-07 14:05:12 -0500

    Assigned appropriate variables for parent classes for my Monopoly game.

--- ICS4U_RST/src/Player.java
+++ ICS4U_RST/src/Player.java
@@ -1,6 +1,12 @@
 
 public class Player {
-
+	
+	private int cash;
+	private int position;
+	private boolean inPrison;
+	private String userName;
+	private boolean isAlive;
+	
 	public Player() {
 		// TODO Auto-generated constructor stub
 	}
