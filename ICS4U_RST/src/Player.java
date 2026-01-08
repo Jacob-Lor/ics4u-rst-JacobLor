3b9e65b848c55fc255925f994b250f70f8c7b762
Jacob Lor <jacob.lor.main@gmail.com>
2026-01-08 13:31:22 -0500

    Completed PlayerObject. Modified its getters and setters slightly the optimize it. I"ll update my design soon after I get my roughwork for the code done. So far player is complete.

--- ICS4U_RST/src/Player.java
+++ ICS4U_RST/src/Player.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Player {
 	
@@ -6,9 +7,25 @@ public class Player {
 	private boolean inPrison;
 	private String userName;
 	private boolean isAlive;
+	private ArrayList<Property> properties = new ArrayList<Property>();
 	
 	public Player() {
 		// TODO Auto-generated constructor stub
+	}
+	public void setCash(int amount) {
+		cash -= amount;
+	}
+	public void setName(String name) {
+		userName = name;
+	}
+	public String getName() {
+		return userName;
+	}
+	public boolean removeProperty(Property p) {
+		return properties.remove(p);
+	}
+	public boolean addProperty(Property p) {
+		return properties.add(p);
 	}
 
 }
